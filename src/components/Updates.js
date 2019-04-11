4bdcded164dc68fe7327537bb3dd84730a979e66
Student <student@Admins-iMac.local>
2019-04-11 16:52:54 -0500

    card color

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -3,43 +3,174 @@ import Carousel from "react-bootstrap/Carousel";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
+
+
 export default class ControlledCarousel extends React.Component {
-  constructor(props, context) {
-    super(props, context);
 
-    this.handleSelect = this.handleSelect.bind(this);
+    constructor(props, context) {
 
-    this.state = {
-      index: 0,
-      direction: null
-    };
-  }
+        super(props, context);
 
-  handleSelect(selectedIndex, e) {
-    this.setState({
-      index: selectedIndex,
-      direction: e.direction
-    });
-  }
 
-  render() {
-    const { index, direction } = this.state;
 
-                            </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+        this.handleSelect = this.handleSelect.bind(this);
+
+
+
+        this.state = {
+
+            index: 0,
+
+            direction: null
+
+        };
+
+    }
+
+
+
+    handleSelect(selectedIndex, e) {
+
+        this.setState({
+
+            index: selectedIndex,
+
+            direction: e.direction
+
+        });
+
+    }
+
+
+
+    render() {
+
+        const { index, direction } = this.state;
+
+
+
+        return (
+
+            <div
+
+                style={{
+
+                    alignItems: "center"
+
+                }}
+
+            >
+
+                <h1
+
+                    style={{
+
+                        color: "white"
+
+                    }}
+
+                >
+
+                    {" "}
+
+                    Title{" "}
+
+                </h1>{" "}
+
+                <br />
+
+                <Carousel
+
+                    activeIndex={index}
+
+                    direction={direction}
+
+                    onSelect={this.handleSelect}
+
+                >
+
+                    <Carousel.Item>
+
+                        <img
+
+                            width={900}
+
+                            height={500}
+
+                            src={require("../images/Uptown.jpg")}
+
+                            alt="First slide"
+
+                        />
+
+                        <Carousel.Caption>
+
+                            <h3> Stage 1 </h3> <p> </p>{" "}
+
+                        </Carousel.Caption>{" "}
+
+                    </Carousel.Item>{" "}
+
+                    <Carousel.Item>
+
+                        <img
+
+                            width={900}
+
+                            height={500}
+
+                            src={require("../images/abandon6.jpg")}
+
+                            alt="Third slide"
+
+                        />
+
+                        <Carousel.Caption>
+
+                            <h3> Stage 2 </h3> <p> </p>{" "}
+
+                        </Carousel.Caption>{" "}
+
+                    </Carousel.Item>{" "}
+
+                    <Carousel.Item>
+
+                        <img
+
+                            width={900}
+
+                            height={500}
+
+                            src={require("../images/abandon4.jpg")}
+
+                            alt="Third slide"
+
+                        />
+
+                        <Carousel.Caption>
+
+                            <h3> Stage 3 </h3> <p />{" "}
+
+                        </Carousel.Caption>{" "}
+
+                    </Carousel.Item>{" "}
+
+                </Carousel>{" "}
+
                 <br />
                 <Card bg="secondary" style={{ justifyContent: 'center', marginLeft: '8%', marginRight: '8%' }}>
-                    <Card.Header>Header</Card.Header>
-                    <Card.Body>
-                        <Card.Text>
-                            Some quick example text to build on the card title
+                <Card.Header>Header</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title
       </Card.Text>
-      
-                    </Card.Body>
-                </Card>
+
+                </Card.Body>
+            </Card>
             </div>
+
         );
-    };
+
+    }
+
 }
