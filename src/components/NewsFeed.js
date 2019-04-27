aeef807ef31bc937b4cdb75c655f6e412324a045
mbanks94 <mbanks123456@gmail.com>
2019-04-27 14:47:52 -0500

    cam's not here anymore

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -55,31 +55,6 @@ export default class NewsFeed extends Component {
     const { shown } = this.state;
     return (
       <>
-        <button onClick={() => {
-          this.switchFeed();
-          if (this.state.idea === true) {
-            this.setState({ idea: false });
-          } else {
-
-            this.setState({ idea: true });
-          }
-          
-        }}></button>
-        {/* <Row style={{ display: 'flex', flexDirection: 'row', alignItems: 'right', justifyContent: 'flex-end', margin: 10 }}>
-          <DropdownButton style={{  marginRight: 30, radius: 10 }} size="lg" id="dropdown-custom-1" title="Filter"  >
-            <Dropdown.Item eventKey="1">Hoover</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item eventKey="2">Downtown</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item eventKey-="3">Uptown</Dropdown.Item>
-          </DropdownButton>
-          <DropdownButton  style={{ fontColor:"white", backgroundColor: '#4B3572', marginRight: 60, borderRadius: 10 }} size="lg" id="dropdown-basic" title="Sort">
-            <Dropdown.Item eventKey="1">Most Upvotes</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item eventKey="2">Recent</Dropdown.Item>
-          </DropdownButton>
-        </Row> */}
-
         <div className="container">
           {this.props.idea === true && <h3>Ideas</h3>}
           {this.props.idea === false && <h3>Updates</h3>}
@@ -90,14 +65,6 @@ export default class NewsFeed extends Component {
           </Row>
           <hr />
         </div>
-        {/* <div className="container">
-          Updates
-        <Row style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 20 }}>
-          {updates.map((info, i) => {
-            return <FeedCard info={info} key={i} />
-          })}
-        </Row>
-        </div> */}
       </>
     )
   }
